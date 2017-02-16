bc4f619824ecab79143325a9a4f64cba492ea9a8
Mohamed Essid <essid.med@gmail.com>
2017-02-16 11:22:37 +0100

    review MainActivityTest - remove unused imports

--- app/src/androidTest/java/com/leadit/androidtesting/MainActivityTest.java
+++ app/src/androidTest/java/com/leadit/androidtesting/MainActivityTest.java
@@ -1,19 +1,12 @@
 package com.leadit.androidtesting;
 
 import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.EditText;
 
-import com.leadit.androidtesting.matcher.HintMatcher;
 import com.leadit.androidtesting.matcher.ToastMatcher;
 import com.leadit.androidtesting.util.Constants;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,13 +14,10 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Checks.checkArgument;
-import static android.support.test.espresso.intent.Checks.checkNotNull;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.leadit.androidtesting.matcher.CustomMatchers.withItemHint;
-import static org.hamcrest.CoreMatchers.is;
 
 /**
  * {@link MainActivity} test class with Espresso framework
